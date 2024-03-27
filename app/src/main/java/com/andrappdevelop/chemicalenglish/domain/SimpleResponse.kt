b0e55c0f6e26f6043f0e6f337a89c8706a70f067
IvanUsatov <ivan.usatov@mechatronica-pro.com>
2024-03-27 11:41:37 +0700

    edit json

--- app/src/main/java/com/andrappdevelop/chemicalenglish/domain/SimpleResponse.kt
+++ app/src/main/java/com/andrappdevelop/chemicalenglish/domain/SimpleResponse.kt
@@ -3,10 +3,10 @@ package com.andrappdevelop.chemicalenglish.domain
 import com.google.gson.annotations.SerializedName
 
 data class SimpleResponse(
-    @SerializedName("id")
-    val id: Int,
+//    @SerializedName("id")
+//    val id: Int,
     @SerializedName("eng")
     val englishWord: String,
-    @SerializedName("ru")
-    val russianWord: String
+//    @SerializedName("ru")
+//    val russianWord: String
 )
